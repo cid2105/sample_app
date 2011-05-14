d5b6d557d7e84b41b3f8dbfd329d0434587902f5
Cole Diamond <cd1am0nd@yahoo.com>
2011-05-14 00:39:15 -0400

    Done with user edit/update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,32 @@
 class UsersController < ApplicationController
+    before_filter :authenticate, :only => [:index, :edit, :update]
+    before_filter :correct_user, :only => [:edit, :update]
+    before_filter :admin_user,   :only => :destroy
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
   
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+  
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(params[:user])
+        flash[:success] = "Profile updated."
+        redirect_to @user
+      else
+        @title = "Edit user"
+        render 'edit'
+      end
+    end
+    
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -20,7 +47,26 @@ class UsersController < ApplicationController
         @title = "Sign up"
         render 'new'
       end
-    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
   
 
+    private
+
+      def authenticate
+        deny_access unless signed_in?
+      end
+      
+      def admin_user
+        redirect_to(root_path) unless current_user.admin?
+      
+      def correct_user
+           @user = User.find(params[:id])
+           redirect_to(root_path) unless current_user?(@user)
+         end
 end
