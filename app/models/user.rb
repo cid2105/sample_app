fad036d7adb556b763728e34bc62948a605eea69
Cole Diamond <cd1am0nd@yahoo.com>
2011-05-14 22:00:30 -0400

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   has_many :microposts, :dependent => :destroy
   has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
   has_many :following, :through => :relationships, :source => :followed
-  has_many :reverse_relationships, :foreign_ley => "followed_id", :class_name => "Relationship", dependent => :destroy
+  has_many :reverse_relationships, :foreign_key => "followed_id", :class_name => "Relationship", :dependent => :destroy
   has_many :followers, :through => :reverse_relationships, :source => :follower
   # Automatically create the virtual attribute 'password_confirmation'.
   validates :password, :presence     => true,
